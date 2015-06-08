4a8b52442ae011f6784faf302952e21e291b9a60
iwillhappy1314 <iwillhappy1314@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-08 16:06:09 +0000

    修正插件启用时2字符异常输出的问题
    git-svn-id: https://plugins.svn.wordpress.org/wizhi-banner-image/trunk@1176828 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- banner-images.php
+++ banner-images.php
@@ -11,107 +11,109 @@ Plugin Type:        Piklist
 License URI:        http://opensource.org/licenses/MIT
 Text Domain:        wizhi
 */
-?>
 
 
-<?php
+/**
+ * Load translations
+ */
+add_action('plugins_loaded', 'wizhi_banner_load_textdomain');
+function wizhi_banner_load_textdomain() {
+	load_plugin_textdomain( 'wizhi', false, basename(dirname(__FILE__)) . '/lang/' );
+}
 
-  /**
-  * Load translations
-  */
-  add_action('plugins_loaded', 'wizhi_banner_load_textdomain');
-  function wizhi_banner_load_textdomain() {
-      load_plugin_textdomain( 'wizhi', false, basename(dirname(__FILE__)) . '/lang/' );
-  }
 
-	//get all custom taxonomy
-	add_filter('piklist_add_part', 'wizhi_assign_all_public_tax', 10, 2);
-	function wizhi_assign_all_public_tax($data, $type){
+/**
+ * get all custom taxonomy
+ */
+add_filter('piklist_add_part', 'wizhi_assign_all_public_tax', 10, 2);
+function wizhi_assign_all_public_tax($data, $type){
 
-		// if is not term return it
-		if($type != 'terms')
-		{
-			return $data;
-		}
-	 
-	    $args = array(
-	       'public'   => true,
-	       '_builtin' => false
-	    );
-	 
-	    $output = 'objects';
-	 
-	    // get all custom taxonomy
-	    $taxonomies = get_taxonomies($args, $output);
-	 
-	    // get taxonomy names
-	    foreach ($taxonomies  as $taxonomy)
-	    {
-	      $taxonomy_list[] = $taxonomy->name;
-	    }
-	 
-	    // get piklist need`s string
-	    $comma_list = implode(', ', $taxonomy_list);
-
-	    //Add the default category
-		$comma_list .= ', category';
-	 
-	    // add all custom taxonomy to piklist
-	    $data['taxonomy'] = $comma_list;
-	 
-	  return $data;
+	// if is not term return it
+	if($type != 'terms') {
+		return $data;
 	}
 
+	$args = array(
+		'public'   => true,
+		'_builtin' => false
+	);
 
-	//Display banner images
-	function wizhi_banner_image(){
+	$output = 'objects';
 
-		if ( is_page() ) {
+	// get all custom taxonomy
+	$taxonomies = get_taxonomies($args, $output);
 
-			global $post;
+	// get taxonomy names
+	foreach ($taxonomies  as $taxonomy) {
+		$taxonomy_list[] = $taxonomy->name;
+	}
 
-			$post_id = $post->ID;
-			$subimg = get_post_meta($post_id, 'subimg', true);
+	// get piklist need`s string
+	$comma_list = implode(', ', $taxonomy_list);
 
-			if( $subimg ){
-				$subimg = $subimg;
-			} else {
-				$post_id = $post->post_parent;
-				$subimg = get_post_meta($post_id, 'subimg', true);
-			}
+	//Add the default category
+	$comma_list .= ', category';
 
-		} elseif( is_single() ){
+	// add all custom taxonomy to piklist
+	$data['taxonomy'] = $comma_list;
 
-			//get post_type`s taxonomy
-			$post_type = $post->post_type;
-			$taxonomies = array_values( get_object_taxonomies( $post_type, 'objects' ) ); #associated array to index array
-			$taxonomy = $taxonomies[0]->name;
+	return $data;
+}
 
-			//get the first term
-			$terms = get_the_terms($post->ID, $taxonomy);
-	    	$term_id = $terms[0]->term_id;
 
-			$subimg = get_term_meta( $term_id, 'subimg', true );
+/**
+ * @return string attachment images src
+ */
+function wizhi_banner_image(){
 
-		} elseif ( is_tax() || is_category() ){
+	global $post;
 
-			$queried_object = get_queried_object();
-			$term_id = $queried_object->term_id;
+	if ( is_page() ) {
 
-			$subimg = get_term_meta( $term_id, 'subimg', true );
+		$post_id = $post->ID;
+		$subimg = get_post_meta($post_id, 'subimg', true);
 
+		if( $subimg ){
+			$subimg = $subimg;
 		} else {
-
-			$subimg = null;
-
+			$post_id = $post->post_parent;
+			$subimg = get_post_meta($post_id, 'subimg', true);
 		}
 
-		$subimg_src = wp_get_attachment_url( $subimg );
+	} elseif( is_single() ){
 
-		if ( $subimg_src ) {
-      return $subimg_src;
-    }
+		//get post_type`s taxonomy
+		$post_type = $post->post_type;
+		$taxonomies = array_values( get_object_taxonomies( $post_type, 'objects' ) ); #associated array to index array
+		$taxonomy = $taxonomies[0]->name;
+
+		//get the first term
+		$terms = get_the_terms($post->ID, $taxonomy);
+		$term_id = $terms[0]->term_id;
+
+		$subimg = get_term_meta( $term_id, 'subimg', true );
+
+	} elseif ( is_tax() || is_category() ){
+
+		$queried_object = get_queried_object();
+		$term_id = $queried_object->term_id;
+
+		$subimg = get_term_meta( $term_id, 'subimg', true );
+
+	} else {
+
+		$subimg = null;
 
 	}
-	
+
+	$subimg_src = wp_get_attachment_url( $subimg );
+
+	if ( $subimg_src ) {
+		return $subimg_src;
+	} else {
+		return '';
+	}
+
+}
+
 ?>
